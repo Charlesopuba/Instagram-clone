67caafdb2ef33740ca380c4ff9e5ab2cadf61a06
Charlesopuba <charles.opuba@student.moringaschool.com>
2021-09-12 22:57:28 +0300

    bootstrap

--- Photogram/settings.py
+++ Photogram/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'insta.apps.InstaConfig'
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -63,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
