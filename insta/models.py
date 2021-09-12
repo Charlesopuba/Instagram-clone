9315795e4c6a4d5f30e6cbf8e7a1afb763ea0539
Charlesopuba <charles.opuba@student.moringaschool.com>
2021-09-12 22:34:41 +0300

    admin andmodels

--- insta/models.py
+++ insta/models.py
@@ -3,18 +3,37 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 ## Feature to enable posting of photos
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(blank=True,upload_to = 'images/')
+    bio = models.CharField(max_length = 255)
+
+    def __str__(self):
+        return f'{self.user.username}'
+
 class Post(models.Model):
-    image = models.ImageField()
-    description = models.TextField
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    created = models.DateField(auto_now_add=True)
-    modified = models.DateField(auto_now=True)
+    pic = models.ImageField(upload_to = 'posts/')
+    caption = models.CharField(blank=True,max_length = 255)
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    like = models.IntegerField(default=0)
 
-## Enable commenting on photos
+    def __str__(self):
+        return f'{self.profile.user.username}'
+
+class Following(models.Model):
+    username = models.CharField(blank=True,max_length = 255)
+    followed = models.CharField(blank=True,max_length = 255)
+
+    def __str__(self):
+        return f'{self.username}'
+
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    text = models .TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    modified = models.DateField(auto_now=True)
+    post = models.IntegerField(default=0)
+    username = models.CharField(blank=True,max_length = 255)
+    comment = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    count = models.IntegerField(default=0)
 
+    def __str__(self):
+        return f'{self.username}'
 
